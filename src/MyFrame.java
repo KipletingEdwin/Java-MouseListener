519190ff21c38bdd7a7960df53747a1e0c22c999
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-06-27 12:16:29 +0300

    add a label

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,12 +1,46 @@
 import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class MyFrame extends  JFrame {
+public class MyFrame extends  JFrame implements MouseListener {
+
+    JLabel label;
 
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 500);
         this.setLayout(null);
+
+
+        label = new JLabel();
+        label.setBounds(0, 0, 100, 100);
+
+
         this.setVisible(true);
     }
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
